a88a50ff25edce871e0d28c4d6df49e40feab8cc
thetruerailen <thetruerailen@users.noreply.github.com>
2023-11-14 21:48:14 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="thetruerailen"
+original_name="pandaapi"
+original_urlname="PandaAPI"
+original_description="Awesome pandaapi created by thetruerailen"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pandaapi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- pandaapi/app.py
+++ pandaapi/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+pandaapi API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="pandaapi",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://pandaapi.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "thetruerailen",
+        "url": "http://pandaapi.com/contact/",
+        "email": "thetruerailen@pandaapi.com",
     },
     license_info={
         "name": "The Unlicense",
